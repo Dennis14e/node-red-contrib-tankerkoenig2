4a40a0a5d54e809442c0a7016ffd172234e7bafd
Dennis Neufeld <git@dneufeld.net>
2021-12-29 15:17:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-red-contrib-tankerkoenig2
+
+Node-RED nodes to receive fuel prices and gas station informations from tankerkoenig.de.
---
+++ src/tankerkoenig.js
@@ -0,0 +1,22 @@
+module.exports = function (RED) {
+    function Tankerkoenig2Config (config) {
+        RED.nodes.createNode(this, config);
+
+        this.name = config.name;
+        this.key = config.key;
+    }
+
+    RED.nodes.registerType('tankerkoenig2-config', Tankerkoenig2Config)
+
+
+    function Tankerkoenig2Radius (config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.on('input', function (msg) {
+            msg.payload = config;
+        });
+    }
+
+    RED.nodes.registerType('tankerkoenig2-radius', Tankerkoenig2Radius);
+}
